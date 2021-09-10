11759d86ab69d13722c3002143589959e80c74d6
EC2 Default User <ec2-user@ip-172-31-29-201.us-east-2.compute.internal>
2021-09-10 03:03:22 +0000

    first commit

--- lib/cdk-custom-resource-cdkpipeline-stack.ts
+++ lib/cdk-custom-resource-cdkpipeline-stack.ts
@@ -1,9 +1,58 @@
 import * as cdk from '@aws-cdk/core';
 
-export class CdkCustomResourceCdkpipelineStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as customresources from '@aws-cdk/custom-resources';
 
-    // The code that defines your stack goes here
-  }
+export class CdkCustomResourceCdkpipelineStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        /*
+        https://baihuqian.github.io/2020-12-17-lambda-based-cdk-custom-resource-with-input-and-output/ 
+        https://github.com/shankben/firebase-migrator/blob/76ebb0bbf12678f41ddfb37b87fa7e9501d666d1/lib/constructs/sync-machine-execution.ts 
+        https://github.com/shankben/firebase-migrator/blob/76ebb0bbf1/assets/lambda/sync/execution/index.ts#L10-L12
+        */
+
+        const lambdaCode = `
+import boto3
+import botocore
+import json
+
+ddb_client = boto3.client('dynamodb')
+
+def handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+
+    props = event['ResourceProperties']
+  
+    output = {
+        'Data': {
+            'MyAtt': 'test1'
+        }
+    }
+    print("Output: " + json.dumps(output))
+    return output
+
+    `;
+
+        const customFunction = new lambda.Function(this, `DdbStreamFn`, {
+            runtime: lambda.Runtime.PYTHON_3_7,
+            code: lambda.Code.fromInline(lambdaCode),
+            handler: 'index.handler',
+        });
+
+        const provider = new customresources.Provider(this, "Provider", {
+            onEventHandler: customFunction,
+        });
+
+        const customResource = new cdk.CustomResource(this, "CustomResource", {
+            serviceToken: provider.serviceToken,
+            resourceType: "Custom::Test",
+        });
+
+        const output = new cdk.CfnOutput(this, "OutputTest", {
+            value: customResource.getAttString("MyAtt"),
+        });
+
+    }
 }
